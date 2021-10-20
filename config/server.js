363b7e02e8833603d93f57d57e78c8ddc95fd622
uzor13 <p.uzor99@gmail.com>
2021-10-20 09:01:27 +0100

    backend initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9415652414daaf489f6e3695bda0f41d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b492edac-c00d-4226-9c8a-1abfdf88b1cf'
+};
